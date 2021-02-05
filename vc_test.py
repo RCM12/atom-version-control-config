e09dbf49b766da514374b3768a68c454db219029
Ronan Manvelian <ronanmanvelian@gmail.com>
2021-02-04 22:31:13 -0500

    Added functions main() and calculate_sum()

---
+++ vc_test.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    answer = a + b
+    return answer
+
+def main():
+    operand1 = int(input("Enter the first number: "))
+    operand2 = int(input("Enter the second number: "))
+    sum_result = calculate_sum(operand1, operand2)
+    print(sum_result)
+
+main()
